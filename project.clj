36b83023621bbc5f64fb56ce03cafadbe6fe44c4
Steve Walker <swalker@walkertek.com>
2020-08-06 16:54:13 -0400

    update project

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject clj-oauth "1.5.7-SNAPSHOT"
+(defproject org.clojars.stw/clj-oauth "1.5.7"
   :url "https://github.com/stw/clj-oauth"
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :creds :gpg}]] 
   :license {:name "Simplified BSD License"
             :url "https://opensource.org/licenses/BSD-2-Clause"
             :distribution :repo}
-  :description "OAuth support for Clojure"
+  :description "OAuth support for Clojure, with HMAC-SHA256"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [commons-codec/commons-codec "1.8"]
                  [org.bouncycastle/bcprov-jdk15on "1.54"]
